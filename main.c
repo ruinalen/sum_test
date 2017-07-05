5ee538273ed11e4e1adc06576aa68c0baad35d1a
ruinalen <ruinalen@gmail.com>
2017-07-05 18:41:59 +0900

    Edit all modules

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "sum.h"
+int main(){
+	int s = 100;
+	printf("%d\n",sum(s));
+
+	return 0;
+}
+
---
+++ sum.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "sum.h"
+int sum(int n){
+	int res=0;
+	for(int a=1;a<=n;a++)
+		res+=a;
+	return res
+}
